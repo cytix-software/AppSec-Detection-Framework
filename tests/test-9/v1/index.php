a068b9e8d3b9c348169b793a156cb326bfd1c79b
Boo-LU <b.s.smith@lancaster.ac.uk>
2025-01-15 14:36:19 +0000

    Updated index.php for path traversal.

--- tests/test-9/v1/index.php
+++ tests/test-9/v1/index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Cheese is the best";
+$filename = $_GET['path'] ?? '';
+$filePath = __DIR__ . '/path/' . $filename;
+
+if (is_file($filePath)) {
+    echo nl2br(htmlspecialchars(file_get_contents($filePath)));
+} else {
+    echo "File not found or invalid file.";
+}
 ?>
