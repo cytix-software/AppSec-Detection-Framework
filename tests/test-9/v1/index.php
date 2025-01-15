834c65b2d1f997819c3cc4e54b4753f0199370df
Boo-LU <b.s.smith@lancaster.ac.uk>
2025-01-15 15:25:17 +0000

    edited index.php and renamed folder.

--- tests/test-9/v1/index.php
+++ tests/test-9/v1/index.php
@@ -1,10 +1,22 @@
-<?php
-$filename = $_GET['path'] ?? '';
-$filePath = __DIR__ . '/path/' . $filename;
+<form action="" method="post"> 
+    <input type="text" name="input" id="input" value="readme.txt">
+    <input type="submit" value="Submit">
+</form>
 
-if (is_file($filePath)) {
-    echo nl2br(htmlspecialchars(file_get_contents($filePath)));
-} else {
-    echo "File not found or invalid file.";
-}
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // User input is directly used to construct the file path.
+        $filename = "./uploads/";  // Intended directory for file access
+        $filename .= $_POST['input'];  // Append user input to path
+
+        // Attempt to open the file based on user input
+        if (is_file($filename)) {
+            $handle = fopen($filename, 'r');
+            $output = fread($handle, filesize($filename));
+            fclose($handle);
+            echo nl2br(htmlspecialchars($output));  // Display file contents
+        } else {
+            echo "File not found or invalid file.";
+        }
+    }
 ?>
