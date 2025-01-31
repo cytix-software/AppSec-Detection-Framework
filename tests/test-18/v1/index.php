ca23dbe45fbd6521b077296cac70be4546253097
Boo-LU <b.s.smith@lancaster.ac.uk>
2025-01-31 12:07:37 +0000

    Removed cwe-117.

--- tests/test-18/v1/index.php
+++ tests/test-18/v1/index.php
@@ -1,17 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $val = $_POST['val']; 
-    try {
-        if (!is_numeric($val)) {
-            throw new Exception("Invalid number");
-        }
-        $value = (int) $val; 
-    } catch (Exception $e) {
-        // CWE-117: Log Injection 
-        $decoded = urldecode($val);
-        $logEntry = "INFO: Failed to parse val = $decoded\n"; 
-        file_put_contents("app.log", $logEntry, FILE_APPEND); 
-    }
     
     // CWE-644: HTTP Header Injection 
     header("X-Custom-Header: $val"); 
@@ -28,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CWE-644, CWE-116 & CWE-117 Vulnerability Demo</title>
+    <title>Test 18</title>
 </head>
 <body>
     <h2>Enter a Value</h2>
