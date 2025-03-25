17d7b0d8aba1364f34e4490a6f9c9a4e2078dbf6
RichardOven <168428600+Cytix-RichardOven@users.noreply.github.com>
2025-03-25 15:18:35 +0000

    refactor(UI): :recycle: Clean up package.json and the base setup

--- visualizer/src/main.ts
+++ visualizer/src/main.ts
@@ -1,16 +1,8 @@
 import './assets/main.css'
-import { createApp } from 'vue';
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'primeicons/primeicons.css';
 
-const app = createApp(App);
+const app = createApp(App)
 app.use(router)
-app.use(PrimeVue,{
-    theme: {
-        preset: Aura
-    }
-});
 app.mount('#app')
