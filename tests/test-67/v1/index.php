ff1a98cddcdfd100e7cd923e00d1efe95f022807
hothersj <j.hothersall@lancaster.ac.uk>
2026-02-25 18:05:48 +0000

    Fix test 67v1 not running

--- tests/test-67/v1/index.php
+++ tests/test-67/v1/index.php
@@ -1,17 +1,40 @@
 <?php
-                                                                                                                                                                                                                   
+$out = '';
+$err = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $code = $_POST['code'] ?? '';
+
+    ob_start();
+    try {
+        eval($code);
+    } catch (Throwable $e) {
+        $err = $e->getMessage();
+    }
+    $out = ob_get_clean();
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Interactive App</title>
+  <meta charset="UTF-8">
+  <title>Interactive App</title>
 </head>
 <body>
-    <h1>Interactive App</h1>
-    <form method="post">
-        <label>Enter PHP to evaluate:</label><br>
-        <input type="text" name="code" style="width:400px" required><br>
-        <button type="submit">Run</button>
-    </form>
+  <h1>Interactive App</h1>
+
+  <form method="post">
+      <label>Enter PHP to evaluate:</label><br>
+      <input type="text" name="code" style="width:400px" required><br>
+      <button type="submit">Run</button>
+  </form>
+
+  <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+    <h3>Eval result</h3>
+    <?php if ($err): ?>
+      <pre style="color:red;"><?php echo htmlspecialchars($err); ?></pre>
+    <?php endif; ?>
+    <pre><?php echo htmlspecialchars($out); ?></pre>
+  <?php endif; ?>
 </body>
 </html>
