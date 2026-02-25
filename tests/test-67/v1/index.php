0df21ca8746a2d83a88f964fbb783bd12ceb8d2f
hothersj <j.hothersall@lancaster.ac.uk>
2026-02-25 17:42:27 +0000

    Fix hint in test 67v1

--- tests/test-67/v1/index.php
+++ tests/test-67/v1/index.php
@@ -1,23 +1,17 @@
 <?php
-// This application evaluates user input.
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $input = $_POST['code'] ?? '';
-    echo "<b>Input:</b> " . htmlspecialchars($input) . "<br>";
-    eval($input);
-}
+                                                                                                                                                                                                                   
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Code Injection (eval)</title>
+    <title>Interactive App</title>
 </head>
 <body>
-    <h1>Code Injection (eval)</h1>
+    <h1>Interactive App</h1>
     <form method="post">
-        <label>Enter PHP code to evaluate (e.g., system('id');):</label><br>
+        <label>Enter PHP to evaluate:</label><br>
         <input type="text" name="code" style="width:400px" required><br>
-        <button type="submit">Run Code</button>
+        <button type="submit">Run</button>
     </form>
 </body>
 </html>
