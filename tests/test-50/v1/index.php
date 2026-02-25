93dc1c25ebfcf95ebe263de35c2f0076d699a06a
hothersj <j.hothersall@lancaster.ac.uk>
2026-02-25 16:19:49 +0000

    Test 50 reflects origin to allow credentials instead of '*'

--- tests/test-50/v1/index.php
+++ tests/test-50/v1/index.php
@@ -1,9 +1,11 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+header("Access-Control-Allow-Origin: $origin"); //reflect origin
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Credentials: true");
+header("Vary: Origin");
 
 // Sample sensitive data
 $sensitiveData = [
